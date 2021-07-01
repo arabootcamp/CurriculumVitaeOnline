68c5016ba8a65a4f3a380ab88900f45aa15b36d3
aracelly <aracellybootcamp@gmail.com>
2021-06-30 22:26:50 -0400

    avance en diseño movil y desktop se debe solucionar tema de posicionamiento navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,38 @@
-$(function() {
+$(function () {
   // Handler for .ready() called.
 
+  const large = 991.98;
+
+  //load
+  if ($(window).width() >= large) {
+    let wNavbar = $('.navbar-custom').width();
+    let wNavbarFixed = $('.navbar-custom__fixed').width();
+    console.log("navbar-custom: " + wNavbar + "navbar__lgnavbar-custom__fixed: " + wNavbarFixed);
+    let left = (wNavbar - wNavbarFixed) / 2;
+    console.log("left: " + left);
+    $('.navbar-custom__fixed').css("left", left);
+  }
+
+  //resize
+  $(window).resize(function () {
+    if ($(window).width() >= large) {
+      let wNavbar = $('.navbar-custom').width();
+      let wNavbarFixed = $('.navbar-custom__fixed').width();
+      console.log("navbar-custom: " + wNavbar + "navbar__lgnavbar-custom__fixed: " + wNavbarFixed);
+      let left = (wNavbar - wNavbarFixed) / 2;
+      console.log("left: " + left);
+      $('.navbar-custom__fixed').css("left", left);
+    }
+  });
+
+  //scroll
+  $(window).scroll(function () {
+    console.log ("scroll");
+    if ($(window).width() >= large) {
+      if ($(window).scrollTop() > 54)
+        $('.navbar__lg').removeClass('pt-lg-5');
+      else
+        $('.navbar__lg').addClass('pt-lg-5');
+    }
+  });
 });
